822e7794b35fa49aa665853730b92e476de60266
papertokyo <dave@papertokyo.com>
2019-02-10 01:47:29 +0800

    added Sheets credentials

--- src/importer.js
+++ src/importer.js
@@ -1,2 +1,3 @@
-'1081263189466 - bcj4pu2n68q0jv2svgu8dl6907p0soi7.apps.googleusercontent.com'
-'sJ - AHTsCXatWUqL2g6RVdVrK'
+const credentials = null
+;('1081263189466 - bcj4pu2n68q0jv2svgu8dl6907p0soi7.apps.googleusercontent.com')
+;('sJ - AHTsCXatWUqL2g6RVdVrK')
